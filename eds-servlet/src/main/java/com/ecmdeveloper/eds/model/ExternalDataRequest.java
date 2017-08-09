4ef7a9a5f21403c5b0d9586478b86d64468ff078
Ricardo Belfor <ricardo.belfor@ecmdeveloper.com>
2017-08-09 07:33:30 -0300

    Added some java doc

--- eds-servlet/src/main/java/com/ecmdeveloper/eds/model/ExternalDataRequest.java
+++ eds-servlet/src/main/java/com/ecmdeveloper/eds/model/ExternalDataRequest.java
@@ -4,17 +4,68 @@ import java.util.Map;
 
 import com.ecmdeveloper.eds.model.constants.RequestMode;
 
-public interface ExternalDataRequest {
+/**
+ * The class describes the request that is made to the service. The Iterable interface 
+ * can by used to walk through all the property objects.
+ * @author Ricardo Belfor
+ *
+ */
+public interface ExternalDataRequest extends Iterable<Property> {
 	
-	public String getExternalDataIdentifier(); 
-
+	/**
+	 * The globally unique identifier (GUID) or persistent identifier (PID) that
+	 * identifies the item that is being edited.
+	 * @return the identifier.
+	 */
 	public String getObjectId() ;
 
+	/**
+	 * The name of the target external data store that contains the property data.
+	 * @return the repository id.
+	 */
 	public String getRepositoryId() ;
 
+	/**
+	 * Returns the  reason that the service is being called. The value can be one of these values:
+	 * <ul>
+	 * <li>initialNewObject</li>
+	 * <li>initialExistingObject</li>
+	 * <li>inProgressChanges</li>
+	 * <li>finalNewObject</li>
+	 * <li>finalExistingObject</li>
+	 * </ul>
+	 * @return one of the request modes.
+	 */
 	public RequestMode getRequestMode();
 
+	/**
+	 * An array that contains a series of key value pairs that specify
+	 * contextual information for a specific class or item type. This parameter
+	 * is used to send information to an external data service when an IBM
+	 * Content Navigator user begins to add a document, add a folder, use an
+	 * entry template, or create a search.
+	 * @return a map containing the data.
+	 */
 	public Map<String, Object> getClientContext();
 	
+	/**
+	 * Returns a specific property identified by it's symbolic name.
+	 * @param name the symbolic name of the property.
+	 * @return the property.
+	 */
 	public Property getProperty(String name);
+
+	/**
+	 * A string that indicates the state of the data that was returned by the
+	 * external data service. The request must include this identifier if the
+	 * requestMode parameter is set to one of these values:
+	 * <ul>
+	 * <li>inProgressChanges</li>
+	 * <li>finalNewObject</li>
+	 * <li>finalExistingObject</li>
+	 * </ul>
+	 * This API makes sure it has a default value.
+	 * @return
+	 */
+	public String getExternalDataIdentifier();
 }
--- eds-servlet/src/main/java/com/ecmdeveloper/eds/model/ExternalDataResponse.java
+++ eds-servlet/src/main/java/com/ecmdeveloper/eds/model/ExternalDataResponse.java
@@ -1,7 +1,17 @@
 package com.ecmdeveloper.eds.model;
 
+/**
+ * This class describes the response returned by the service.
+ * @author Ricardo Belfor
+ *
+ */
 public interface ExternalDataResponse {
 
+	/**
+	 * Adds a Property object to the external data response. The property object describes the
+	 * required behavior of the corresponding property.
+	 * @param property the property to add.
+	 */
 	void addProperty(Property property);
 
 	/**
