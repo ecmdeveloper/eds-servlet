9a3d280f8413df34af73a1b1d92d7fe1c93376d4
Ricardo <ricardo.belfor@ecmdeveloper.com>
2020-04-20 08:50:30 +0200

    Added advanced data validation example

--- eds-servlet-advanced-validation/src/main/java/com/ecmdeveloper/eds/validation/servlet/EDSAdvancedValidationServlet.java
+++ eds-servlet-advanced-validation/src/main/java/com/ecmdeveloper/eds/validation/servlet/EDSAdvancedValidationServlet.java
@@ -5,7 +5,6 @@ package com.ecmdeveloper.eds.validation.servlet;
 
 import javax.servlet.annotation.WebServlet;
 
-import org.iban4j.Iban;
 import org.iban4j.IbanFormatException;
 import org.iban4j.IbanUtil;
 import org.iban4j.InvalidCheckDigitException;
@@ -28,64 +27,97 @@ import com.ecmdeveloper.eds.servlet.AbstractEDSServlet;
 		})
 public class EDSAdvancedValidationServlet extends AbstractEDSServlet{
 
+	private static final long serialVersionUID = 0xC00L;
+
 	@Override
 	public String[] getObjectTypeNames(String repositoryId) {
 		return new String[] {"Customer" };
 	}
 
-	private static final long serialVersionUID = -7290500028447822821L;
-
 	@Override
 	public void handleRequest(ExternalDataRequest dataRequest, ExternalDataResponse dataResponse) {
+		handleIbanRequestVersion3(dataRequest, dataResponse);
+	}
+
+	/**
+	 * Simple IBAN validation using a regular expression.
+	 * 
+	 * @param dataRequest the data request
+	 * @param dataResponse the data response
+	 */
+	public void handleIbanRequestVersion1(ExternalDataRequest dataRequest, ExternalDataResponse dataResponse) {
+
+		Property iban = dataRequest.getProperty("DEV_IBAN");
+		
+		if (iban != null) {
+
+			iban.setFormat("([A-Z]{2})(\\d{2})([A-Za-z\\d]+)$");
+			iban.setFormatDescription("<br><ul><li>Country code</li>"
+					+ "<li>Two check digits</li>"
+					+ "<li>Basic Bank Account Number</li></ul>");
+			dataResponse.addProperty(iban);
+		}
+	}
+
+	/**
+	 * Advanced IBAN validation using a validation library.
+	 * 
+	 * @param dataRequest the data request
+	 * @param dataResponse the data response
+	 */
+	public void handleIbanRequestVersion2(ExternalDataRequest dataRequest, ExternalDataResponse dataResponse) {
+
+		Property iban = dataRequest.getProperty("DEV_IBAN");
+
+		if (iban != null) {
+
+			iban.setHasDependentProperties(true);
+
+			String ibanValue = (String)iban.getValue();
+			if ( !ibanValue.isEmpty() ) {
+				try {
+					IbanUtil.validate(ibanValue);
+				} catch (IbanFormatException |
+						InvalidCheckDigitException | 
+						UnsupportedCountryException e ) {
+					iban.setCustomValidationError(e.getMessage() );
+				}
+			}
+			dataResponse.addProperty(iban);
+		}
+	}
+
+	/**
+	 * Advanced IBAN validation with extra feedback in the user interface.
+	 *  
+	 * @param dataRequest the data request
+	 * @param dataResponse the data response
+	 */
+	public void handleIbanRequestVersion3(ExternalDataRequest dataRequest, ExternalDataResponse dataResponse) {
 		
 		Property iban = dataRequest.getProperty("DEV_IBAN");
 		Property validationError = dataRequest.getProperty("ValidationError");
-		
+
 		if (iban != null && validationError != null) {
 
-// Version 1			
-//			iban.setFormat("([A-Z]{2})(\\d{2})([A-Za-z\\d]+)$");
-//			iban.setFormatDescription("<br><ul><li>Country code</li>"
-//					+ "<li>Two check digits</li>"
-//					+ "<li>Basic Bank Account Number</li></ul>");
-//			dataResponse.addProperty(iban);
-			
-			
-// Version 2
-			
-//			iban.setHasDependentProperties(true);
-//			
-//			String ibanValue = (String)iban.getValue();
-//			if ( !ibanValue.isEmpty() ) {
-//				try {
-//					IbanUtil.validate(ibanValue);
-//				} catch (IbanFormatException |
-//						 InvalidCheckDigitException | 
-//						 UnsupportedCountryException e ) {
-//					iban.setCustomValidationError(e.getMessage() );
-//				}
-//			}
-//			dataResponse.addProperty(iban);
+			iban.setHasDependentProperties(true);
 
-// Version 3
-			
-//			iban.setHasDependentProperties(true);
-			
 			validationError.setLabel("IBAN validation");
 			validationError.setDisplayMode(DisplayMode.readonly);
 			validationError.setValue("");
 			validationError.setFormat("^(?![\\s\\S])");
 			dataResponse.addProperty(validationError);
-			
+
 			String ibanValue = (String)iban.getValue();
 			if ( ibanValue != null && !ibanValue.isEmpty() ) {
 				try {
 					IbanUtil.validate(ibanValue);
 				} catch (IbanFormatException |
-						 InvalidCheckDigitException | 
-						 UnsupportedCountryException e ) {
+						InvalidCheckDigitException | 
+						UnsupportedCountryException e ) {
 					iban.setCustomValidationError(e.getMessage() );
 					validationError.setValue(e.getMessage());
+					validationError.setCustomValidationError("Invalid IBAN");
 				}
 			}
 
